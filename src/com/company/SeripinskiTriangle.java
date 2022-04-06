f144a4847e2437c9527c240c0220f9fd24254c53
student <123>
2022-04-06 12:13:48 -0400

    CW

--- src/com/company/SeripinskiTriangle.java
+++ src/com/company/SeripinskiTriangle.java
@@ -16,27 +16,26 @@ public class SeripinskiTriangle extends JPanel
         super.paintComponent(g); // Call JPanel&#39;s paintComponent method to paint the background
         int xCenter = getWidth() / 2;
         int yCenter = getHeight() / 2;
-        int [] xcoord = {xCenter - 128, xCenter-128, xCenter + 128, xCenter + 128};
-        int [] ycoord = {yCenter-128, yCenter + 128, yCenter + 128, yCenter - 128};
+        int [] xcoord = {xCenter - 128, xCenter, xCenter + 128};
+        int [] ycoord = {yCenter, yCenter - 128, yCenter};
         g.setColor(Color.RED);
         drawAndSplit(g, xcoord, ycoord, 1);
     }
     public int [] midpoints(int [] x)
     {
-        int [] m = new int [4];
+        int [] m = new int [3];
         m[0] = (x[0] + x[1])/2;
         m[1] = (x[1] + x[2])/2;
         m[2] = (x[2] + x[3])/2;
-        m[3] = (x[3] + x[0])/2;
         return m;
     }
     public void drawAndSplit(Graphics g, int [] x, int [] y, int times)
     {
         if(times==levels) {
-            g.drawPolygon(x, y, 4);
+            g.drawPolygon(x, y, 3);
         }
         else {
-            g.drawPolygon(x, y, 4);
+            g.drawPolygon(x, y, 3);
             drawAndSplit(g, midpoints(x), midpoints(y), times + 1);
         }
 /****** OOPS! I lost the code ******/
@@ -47,7 +46,7 @@ public class SeripinskiTriangle extends JPanel
         window.setBounds(200, 200, 500, 500);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        SquareRose panel = new SquareRose(10);
+        SeripinskiTriangle panel = new SeripinskiTriangle(2);
         panel.setBackground(Color.WHITE);
         Container c = window.getContentPane();
         c.add(panel);
